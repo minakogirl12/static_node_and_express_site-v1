bf2f8f397549480db78f43523743479f04e42bb0
drobinson <30643831+minakogirl12@users.noreply.github.com>
2021-01-20 17:08:16 -0500

    Update projects.js
    Added a redirect for project ids that do not exist

--- routes/projects.js
+++ routes/projects.js
@@ -2,18 +2,20 @@ const express = require('express');
 const router = express.Router();
 const {data} = require('../data.json'); //converts data to a json object
 const {projects} = data;
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     const {id} = req.params;
     console.log(id);
     
     //verify that project exists
-    const projectData = projects[id-1];
-    console.log(projectData.image_urls);
-    res.render('project', projectData);
-   
-    
-
-    //if project doesn't exist throw an error
+    if(isNaN(id) || id === '0' || id > projects.length){
+            //if project doesn't exist redirect to error page
+           res.redirect('/error');        
+    }
+    else{
+        const projectData = projects[id-1];
+        console.log(projectData.image_urls);
+        res.render('project', projectData);
+    }
 
 });
 
